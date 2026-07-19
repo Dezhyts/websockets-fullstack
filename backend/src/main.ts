e2336a4f0ee101fe1f90fb2879a1a6bd74acd027
Dezhyts <dezzhyts@gmail.com>
2026-07-19 18:30:08 +0300

    chore add dependencies socket.io , redis , redis-adapter etc.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger();
+  const config = app.get(ConfigService);
+
+  const host = config.getOrThrow<string>('HOST') ?? 'localhost';
+  const port = config.getOrThrow<number>('PORT') ?? 3000;
+
+  await app.listen(port);
+  logger.log(`App started on ${host}:${port}`);
 }
 bootstrap();
