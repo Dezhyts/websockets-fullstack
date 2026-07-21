5fe4be52348f3392bdfad9b641dd0bc0d823c73b
Dezhyts <dezzhyts@gmail.com>
2026-07-21 13:25:28 +0300

    feat add: LiveKit module generate key

--- backend/src/modules/media/media.controller.ts
+++ backend/src/modules/media/media.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { MediaService } from './media.service';
+import { GetTokenDto } from './dto/get-token.dto';
 
 @Controller('media')
 export class MediaController {
   constructor(private readonly mediaService: MediaService) {}
+
+  @Get('token')
+  async getToken(@Query() query: GetTokenDto) {
+    return this.mediaService.generateToken(query.roomName, query.user);
+  }
 }
--- backend/src/modules/media/media.service.ts
+++ backend/src/modules/media/media.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AccessToken } from 'livekit-server-sdk';
 
 @Injectable()
-export class MediaService {}
+export class MediaService {
+  constructor(private readonly configService: ConfigService) {}
+
+  async generateToken(roomName: string, participant: string) {
+    const apiKey = this.configService.getOrThrow<string>('LIVEKIT_API_KEY');
+    const apiSecret =
+      this.configService.getOrThrow<string>('LIVEKIT_API_SECRET');
+    const at = new AccessToken(apiKey, apiSecret, {
+      identity: participant,
+    });
+
+    at.addGrant({
+      room: roomName,
+      canPublish: true,
+      canSubscribe: true,
+      roomJoin: true,
+    });
+
+    const tokenString = await at.toJwt();
+
+    return { token: tokenString };
+  }
+}
